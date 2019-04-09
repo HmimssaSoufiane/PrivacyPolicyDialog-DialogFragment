a82c741d9d15f189f29f72e813740f18fdafcb23
hmimssa <hmimssasoufiane@hotmail.com>
2019-04-09 19:58:19 +0200

    Merge branch 'master' of https://github.com/HmimssaSoufiane/PrivacyPolicyDialog
    # Conflicts:
    #	app/src/main/java/com/zsquad/privacypolicy_dialog/MainActivity.java

--- app/src/main/java/com/zsquad/privacypolicy_dialog/MainActivity.java
+++ app/src/main/java/com/zsquad/privacypolicy_dialog/MainActivity.java
@@ -5,6 +5,7 @@ import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.view.Window;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
@@ -13,6 +14,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        getSupportActionBar().hide();
         Button btnAccep;
         Button btnCanal;
         btnAccep= findViewById(R.id.dialogbtn1);
