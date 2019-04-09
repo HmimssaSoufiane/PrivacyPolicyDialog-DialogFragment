50783b71d2cc563043bb6a6fae5145d624101ea1
hmimssa <hmimssasoufiane@hotmail.com>
2019-04-09 19:48:13 +0200

    add xml dialog script and dimens file

--- app/src/main/java/com/zsquad/privacypolicy_dialog/MainActivity.java
+++ app/src/main/java/com/zsquad/privacypolicy_dialog/MainActivity.java
@@ -1,11 +1,7 @@
 package com.zsquad.privacypolicy_dialog;
 
-import android.content.Intent;
-import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -13,34 +9,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Button btnAccep;
-        Button btnCanal;
-        btnAccep= findViewById(R.id.dialogbtn1);
-        btnCanal= findViewById(R.id.dialogbtn2);
-        btnAccep.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                //open main activity
-                Intent mainActivity = new Intent(getApplicationContext(),MainActivity.class);
-                startActivity(mainActivity);
-                savePrefsData();
-                finish();
-            }
-        });
-        btnCanal.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                finish();
-            }
-        });
 
-    }
-    private void savePrefsData(){
-
-        SharedPreferences pref = getApplicationContext().getSharedPreferences("myPrefs",MODE_PRIVATE);
-        SharedPreferences.Editor editor = pref.edit();
-        editor.putBoolean("isIntroOpnend",true);
-        editor.commit();
     }
 }
 
