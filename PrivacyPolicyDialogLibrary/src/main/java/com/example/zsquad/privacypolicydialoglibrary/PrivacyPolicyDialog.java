a4fea828e325903b1ad22bd845f08a6207f26d41
hmimssa <hmimssasoufiane@hotmail.com>
2019-04-11 19:54:38 +0200

    finish

--- PrivacyPolicyDialogLibrary/src/main/java/com/example/zsquad/privacypolicydialoglibrary/PrivacyPolicyDialog.java
+++ PrivacyPolicyDialogLibrary/src/main/java/com/example/zsquad/privacypolicydialoglibrary/PrivacyPolicyDialog.java
@@ -3,6 +3,8 @@ package com.example.zsquad.privacypolicydialoglibrary;
 import android.content.ActivityNotFoundException;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
@@ -11,6 +13,7 @@ import android.support.v4.app.DialogFragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.Window;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -34,17 +37,25 @@ public class PrivacyPolicyDialog extends DialogFragment {
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.dialog, container);
 
-
+        //Dialog style
+        getDialog().requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getDialog().getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+        //
         btnAccept = view.findViewById(R.id.dialogbtn1);
         btnIgnore = view.findViewById(R.id.dialogbtn2);
         textViewBrand = view.findViewById(R.id.textViewBrand);
         textViewPrivacyPolicy = view.findViewById(R.id.textViewPrivacyPolicy);
-        final Bundle args = getArguments();
-        textViewBrand.setText(args.getString("storeName"));
-        pref = getActivity().getSharedPreferences("myPrefs", MODE_PRIVATE);
         dialogFragment = this;
 
+        //get store name and privacy url
+        final Bundle args = getArguments();
+        textViewBrand.setText(args.getString("storeName"));
 
+        //SharedPreferences to store dialog show state
+        pref = getActivity().getSharedPreferences("myPrefs", MODE_PRIVATE);
+
+
+        //check dialog state
         if (restorePrefData()) {
             dialogFragment.dismiss();
         }
