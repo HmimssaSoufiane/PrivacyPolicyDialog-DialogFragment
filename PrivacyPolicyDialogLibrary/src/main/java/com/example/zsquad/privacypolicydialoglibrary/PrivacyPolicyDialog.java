b5a2cf32dfae960dbddcf3f3ceab8808cea9705b
hmimssa <hmimssasoufiane@hotmail.com>
2019-04-11 18:42:09 +0200

    fix all errors

--- PrivacyPolicyDialogLibrary/src/main/java/com/example/zsquad/privacypolicydialoglibrary/PrivacyPolicyDialog.java
+++ PrivacyPolicyDialogLibrary/src/main/java/com/example/zsquad/privacypolicydialoglibrary/PrivacyPolicyDialog.java
@@ -1,7 +1,5 @@
 package com.example.zsquad.privacypolicydialoglibrary;
 
-import android.app.Activity;
-import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
